42e66c87f4ab76fdb1c68545189a4dc61288f6eb
BaronVonAlex <aleko.kotliarovi@gmail.com>
2022-11-26 20:15:22 +0400

    Added User List, Changed some visuals

--- app/src/main/java/com/example/aslcav1/activities/MainActivity.java
+++ app/src/main/java/com/example/aslcav1/activities/MainActivity.java
@@ -35,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void setListeners() {
         binding.imageSignOut.setOnClickListener(v -> signOut());
+        binding.fabNewChat.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), UsersActivity.class)));
     }
 
     private void loadUserDetails() {
@@ -58,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
                 .collection(Constants.KEY_COLLECTION_USERS)
                 .document(preferenceManager.getString(Constants.KEY_USER_ID));
         documentReference.update(Constants.KEY_FCM_TOKEN, token)
-                .addOnSuccessListener(unused -> showToast("Token updated successfully"))
                 .addOnFailureListener(e -> showToast("Unable to update Token"));
     }
 
